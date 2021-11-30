e16b1eca98fff94d095dae03fa57ecee246d3ffc
QTpierogi <76211845+QTpierogi@users.noreply.github.com>
2021-12-01 01:00:59 +0500

    Basic game elements
    Implemented basic game elements and controls for double matrix with digits.

---
+++ Determined/Assets/Scripts/MatrixObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MatrixObjectState
+{
+    Active,
+    Chosen,
+    Blocked
+}
+
+public abstract class MatrixObject : MonoBehaviour
+{
+    public MatrixObjectState currentState = MatrixObjectState.Active;
+    public int value;
+    protected LineController lineController;
+
+    protected virtual void Awake()
+    {
+        lineController = FindObjectOfType<LineController>();
+    }
+
+    public virtual void ChooseMatrixObject()
+    {
+        currentState = MatrixObjectState.Chosen;
+        lineController.AddPoint(gameObject.transform);
+    }
+
+    public virtual void MakeMatrixObjectActive()
+    {
+        currentState = MatrixObjectState.Active;
+        lineController.DeletePoint(gameObject.transform);
+    }
+
+    public virtual void BlockMatrixObject()
+    {
+        currentState = MatrixObjectState.Blocked;
+    }
+
+    public void OnMouseEnter()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (currentState == MatrixObjectState.Active)
+                ChooseMatrixObject();
+            else MakeMatrixObjectActive();
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+            ChooseMatrixObject();
+    }
+}
---
+++ Determined/Assets/Scripts/ResultBoard.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
+
+public enum DeterminantType
+{
+    DoubleMatrix,
+    Leibniz,
+    Triangles,
+    Any
+}
+
+public class ResultBoard : MonoBehaviour
+{
+    private List<int> terms;
+    public List<TMP_Text> resultElementsUI;
+    private List<TMP_Text> signElementsUI;
+    private MatrixObject[] matrixObjects;
+    private ButtonsManager buttonsManager;
+
+    public MatrixObject[] objects;
+    public DeterminantType typeOfResult;
+    
+    private void Awake()
+    {
+        terms = new List<int>();
+        resultElementsUI = FindObjectsOfType<TMP_Text>().Where(x => x.gameObject.tag == "Term").OrderBy(x => x.gameObject.name).ToList();
+        signElementsUI = FindObjectsOfType<TMP_Text>().Where(x => x.text == "?").OrderBy(x => x.gameObject.name).ToList();
+        matrixObjects = FindObjectsOfType<MatrixObject>();
+        buttonsManager = GetComponent<ButtonsManager>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            objects = GetChosenMatrixObjects();
+            CalculateAnswerForDouble(objects);
+        }
+    }
+
+    private void UpdateUI()
+    {
+        for (int i = 0; i < terms.Count; i++)
+            resultElementsUI[i].text = terms[i].ToString();
+        if (signElementsUI.Where(x => x.text == "?").Count() != 0)
+            buttonsManager.ActivateSignButtons();
+    }
+
+    private MatrixObject[] GetChosenMatrixObjects()
+    {
+        return matrixObjects.Where(x => x.currentState == MatrixObjectState.Chosen).ToArray();
+    }
+
+    private void CalculateAnswerForDouble(MatrixObject[] objects)
+    {
+        if (objects.Length != 2)
+            //remove one life
+            return;
+        terms.Add(CalculateSimpleDiagonal(objects[0].value, objects[1].value));
+        UpdateUI();
+        if (terms.Count == 2)
+        {
+            buttonsManager.BLockMatrix();
+            buttonsManager.ActivateResultButton();
+        }
+    }
+
+    private int CalculateSimpleDiagonal(int first, int second)
+    {
+        return first * second;
+    }
+
+    private int CalculateDoubleMatrixDeterminant()
+    {
+        return terms[0] - terms[1];
+    }
+
+    public void GetResult()
+    {
+        if (typeOfResult == DeterminantType.DoubleMatrix)
+            GetResultForDoubleMatrix();
+    }
+
+    private void GetResultForDoubleMatrix()
+    {
+        terms.Add(CalculateDoubleMatrixDeterminant());
+        UpdateUI();
+    }
+}
